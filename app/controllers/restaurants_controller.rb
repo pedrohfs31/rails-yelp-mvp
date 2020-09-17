ad7542a7aa8bc8b54018735bd8b160d0d2efd6b8
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-17 14:02:43 -0300

    index html

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,46 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def show
+    # @restaurant = restaurant.find(params[:id])
+  end
+
+  # def edit
+  #   # @restaurant = restaurant.find(params[:id])
+  # end
+
+  # def update
+  #   @restaurant.update(restaurant_params)
+  #   redirect_to restaurant_path(@restaurant)
+  # end
+
+  # def destroy
+  #   @restaurant.destroy
+  #   redirect_to restaurants_path
+  # end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
