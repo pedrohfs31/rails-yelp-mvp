69849b5101e6839e79419373da5ccd9efdcab580
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-17 12:53:19 -0300

    models and seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many  :reviews, dependent: :destroy
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
 end
